062e98e13be6ff4bcf42924d70a58504ecda74e9
DEBNATH MONDAL <83080813+Debnath2005@users.noreply.github.com>
2022-08-08 14:19:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,6 @@
+import React from "react";
 import "./styles.css";
-import React, { useState } from "react";
+import Appcounter from "./components/counter";
 export default function App() {
-  const [count, setCount] = useState(0);
-  return (
-    <div className="App">
-      <h1> Counter App</h1>
-      <h2>{count}</h2>
-      <button className="incrmentBtn" onClick={() => setCount(count + 1)}>
-        Increment
-      </button>
-      <button
-        className="decrementBtn"
-        onClick={() => setCount(count - 1)}
-        disabled={count === 0}
-      >
-        Decrement
-      </button>
-      <button
-        className="resetBtn"
-        onClick={() => setCount(0)}
-        disabled={count === 0}
-      >
-        Reset
-      </button>
-    </div>
-  );
+  return <Appcounter />;
 }
